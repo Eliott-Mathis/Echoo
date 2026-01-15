534df3f6beacceabdb24f7cadd35d074d3fd01e6
Mathis Olaya <150446507+MathisOlaya@users.noreply.github.com>
2026-01-15 16:35:05 +0100

    feat(router): add router on app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,14 @@
-import fastify from "fastify";
+import fastify, { FastifyError } from "fastify";
 import prismaPlugin from "./plugins/prisma";
+import userRoutes from "./modules/auth/auth.routes";
 
 export const app = fastify({
   logger: true,
 });
 
 await app.register(prismaPlugin);
+
+// routing
+await app.register(userRoutes, { prefix: "/auth" });
+
+// Error hanlder
