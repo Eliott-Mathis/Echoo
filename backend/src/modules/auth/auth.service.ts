a02f10a366cfa476df203b4b1e567396dd9bbbf7
Mathis Olaya <150446507+MathisOlaya@users.noreply.github.com>
2026-01-15 16:36:53 +0100

    feat(signup): add signup function

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -6,4 +6,29 @@ import { CreateUserBody } from "./auth.schema";
 
 export class AuthService {
   constructor(private db: PrismaClient) {}
+
+  async signUp({
+    email,
+    password,
+    username,
+    displayName,
+  }: CreateUserBody): Promise<string> {
+    const user = await this.db.user.findUnique({ where: { email } });
+    if (user)
+      throw new Error("Un utilisateur existe déjà avec cette adresse mail");
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const createdUser = await this.db.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        username,
+        displayName,
+      },
+    });
+
+    return createdUser.id;
+  }
 }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,10 @@
+import "dotenv/config";
 import { app } from "./app";
 
 const start = async () => {
   try {
     await app.listen({ port: 3000 });
-    console.log("Serveur running on localhost:30000");
+    console.log("Serveur running on localhost:3000");
   } catch (err) {
     app.log.error(err);
     process.exit(1);
