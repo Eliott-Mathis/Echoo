b0ba7d0de58812c10fd00d316d66b6a1bf5ef985
Mathis Olaya <150446507+MathisOlaya@users.noreply.github.com>
2026-01-15 16:34:24 +0100

    feat(db): update user model with new fields

---
+++ backend/prisma/migrations/20260115150136_adding/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `avatarUrl` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `bio` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `displayName` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `email` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `isVerified` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "avatarUrl" TEXT NOT NULL,
+ADD COLUMN     "bio" TEXT NOT NULL,
+ADD COLUMN     "displayName" TEXT NOT NULL,
+ADD COLUMN     "email" TEXT NOT NULL,
+ADD COLUMN     "isVerified" BOOLEAN NOT NULL,
+ADD COLUMN     "password" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
