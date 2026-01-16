5b973ff66351ee7561ca3c3ce2aedb26a5815364
Mathis Olaya <150446507+MathisOlaya@users.noreply.github.com>
2026-01-16 14:38:38 +0100

    feat(oa2): create component for oa2 authentication button

--- frontend/src/views/SignUp.tsx
+++ frontend/src/views/SignUp.tsx
@@ -1,3 +1,18 @@
+import type { IconType } from "react-icons/lib";
+import { GoogleIcon, Steam, Twitch, DiscordIcon, Apple } from "brand-logos";
+interface OA2Props {
+  icon: IconType;
+  className?: string;
+}
+
+function OAuth2Button({ icon: Icon, className }: OA2Props) {
+  return (
+    <div className="bg-neutral-lowest w-full py-4 flex justify-center rounded-lg border border-neutral-low hover:bg-neutral-highest">
+      <Icon className={`${className}`} size={24} />
+    </div>
+  );
+}
+
 export default function SignUp() {
   return (
     <AuthLayout
