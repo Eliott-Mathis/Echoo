825db3f0129ef1a6d99deb825a8c7e961ab2f243
Mathis Olaya <150446507+MathisOlaya@users.noreply.github.com>
2026-01-16 15:40:27 +0100

    feat: update input component with error handler

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -3,18 +3,32 @@ import { type LucideIcon } from "lucide-react";
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
   icon: LucideIcon;
+  error?: string;
 }
 
-export default function Input({ label, icon: Icon, ...props }: Props) {
+export default function Input({ label, error, icon: Icon, ...props }: Props) {
   return (
     <div className="relative flex flex-col">
-      <p className="absolute left-3 px-2 top-[-12px] bg-darkblue-500 text-input-primary-default-placeholder">
+      { error ?
+        (<div><p className="absolute left-3 px-2 top-[-12px] bg-darkblue-500 text-danger-low">
+        {error}
+      </p>
+      <div className="flex items-center gap-1 border border-danger-low rounded-lg pl-5">
+        <Icon className="text-input-primary-default-icon" />
+        <input className="w-full h-full p-4 outline-none text-lg" {...props} />
+      </div></div>)
+      
+      :
+      ((<div><p className="absolute left-3 px-2 top-[-12px] bg-darkblue-500 text-input-primary-default-placeholder">
         {label}
       </p>
       <div className="flex items-center gap-1 border rounded-lg pl-5">
         <Icon className="text-input-primary-default-icon" />
         <input className="w-full h-full p-4 outline-none text-lg" {...props} />
-      </div>
+      </div></div>)
+      )}
+      
+      
     </div>
   );
 }
