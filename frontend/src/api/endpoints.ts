6993a05b9a04d02e0a59f0f013cf105d02b5e674
Mathis Olaya <150446507+MathisOlaya@users.noreply.github.com>
2026-01-19 09:12:47 +0100

    chore(api): define all endpoints in a frontend file

---
+++ frontend/src/api/endpoints.ts
@@ -0,0 +1,9 @@
+export const API_ROUTES = {
+    auth: {
+        validation: "/auth/email-verification",
+        login: "/auth/login",
+        signup: "/auth/signup",
+        me: "/auth/me"
+    }, 
+
+}
