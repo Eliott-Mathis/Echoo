b09812e98e2fe99799ff0f831dbb4c607ae94bb3
Mathis Olaya <150446507+MathisOlaya@users.noreply.github.com>
2026-01-19 09:13:06 +0100

    feat(hook): create hook with tquery for api call

---
+++ frontend/src/hooks/useAuth.ts
@@ -0,0 +1,20 @@
+import { useMutation } from "@tanstack/react-query";
+import { AuthAPI } from "@/api/auth.api";
+import type { AxiosError } from "axios";
+
+export const useSignUpValidation = (onError?: (message: string) => void) => {
+    return useMutation({
+        mutationFn: AuthAPI.validation,
+        onError: (error: AxiosError<any>) => {
+            const message = error.response?.data.message ?? "Une erreur est survenue";
+            onError?.(message);
+        }
+    })
+    
+}
+
+export const useSignUp = () => {
+    return useMutation({
+        mutationFn: AuthAPI.signUp
+    })
+}
