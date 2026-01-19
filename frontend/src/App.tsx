108ae39a21cd6ec6dbe542f784d99b653ed50c01
Mathis Olaya <150446507+MathisOlaya@users.noreply.github.com>
2026-01-19 12:31:34 +0100

    feat(route): add route for email verification

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Views
 import SignUp from "./views/SignUp";
+import EmailVerification from "./views/EmailVerification";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/signup" Component={SignUp} />
+        <Route path="/email-verification" Component={EmailVerification}/>
       </Routes>
     </BrowserRouter>
   );
