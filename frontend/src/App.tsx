cef9eb6ed96f7929dab7363ea53059cdb0941f50
eliottscherrer <146830321+eliottscherrer@users.noreply.github.com>
2026-01-21 11:27:01 +0100

    feat: wrap Home component in ProtectedRoute for route protection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ import FriendsTab from "./views/home/FriendsTab";
 import DirectMessageTab from "./views/home/DirectMessageTab";
 import DynamiteTab from "./views/home/DynamiteTab";
 import SettingsPopup from "./views/home/SettingsPopup";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 
 function App() {
@@ -18,13 +19,15 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/friends/addfriend" replace />} />
         <Route path="/home" element={<Navigate to="/friends/addfriend" replace />} />
-        <Route element={<Home />}>
-          <Route path="/friends" element={<Navigate to="/friends/addfriend" replace />} />
-          <Route path="/friends/:friendTab" element={<FriendsTab />} />
-          <Route path="/messages" element={<DirectMessageTab />} />
-          <Route path="/messages/:username" element={<DirectMessageTab />} />
-          <Route path="/dynamite" element={<DynamiteTab />} />
-          <Route path="/settings" element={<SettingsPopup />} />
+        <Route element={<ProtectedRoute />}>
+          <Route element={<Home />}>
+            <Route path="/friends" element={<Navigate to="/friends/addfriend" replace />} />
+            <Route path="/friends/:friendTab" element={<FriendsTab />} />
+            <Route path="/messages" element={<DirectMessageTab />} />
+            <Route path="/messages/:username" element={<DirectMessageTab />} />
+            <Route path="/dynamite" element={<DynamiteTab />} />
+            <Route path="/settings" element={<SettingsPopup />} />
+          </Route>
         </Route>
         <Route path="/signup" element={<SignUp />} />
         <Route path="/email-verification" element={<EmailVerification />} />
