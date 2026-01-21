b933df026750ca8ab347dac20b7f0bd0de097440
Mathis Olaya <150446507+MathisOlaya@users.noreply.github.com>
2026-01-21 10:51:32 +0100

    feat: add payement request in frontend

--- frontend/src/views/home/DynamiteTab.tsx
+++ frontend/src/views/home/DynamiteTab.tsx
@@ -1,11 +1,35 @@
+import { createAuthClient } from "better-auth/react"
+import {stripeClient} from "@better-auth/stripe/client"
+import Button from "@/components/Button"
+import { useState } from "react"
+
+const authClient = createAuthClient({
+    baseURL: "http://localhost:3000",
+    plugins: [
+        stripeClient({
+            subscription: true
+        })
+    ]
+})
+
 export default function DynamiteTab() {
+  const [loading, setLoading] = useState(false)
+
+    const handlePayement = async() => {
+        const {data, error} = await authClient.subscription.upgrade({
+            plan: 'Dynamite',
+            annual: false,
+            successUrl: 'http://localhost:5173/dynamite/success',
+            cancelUrl: 'http://localhost:5173/dynamite/cancel',
+            disableRedirect: false
+        })
+    }
+
   return (
     <div className="flex-1 px-10 py-8">
       <div className="max-w-3xl flex flex-col gap-2">
         <h1 className="text-3xl font-bold">Dynamite</h1>
-        <p className="text-sm text-neutral-medium">
-          TODO: build the Dynamite experience here.
-        </p>
+        <Button isLoading={loading} onClick={handlePayement}>Acheter</Button>
       </div>
     </div>
   );
