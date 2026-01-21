9e3e2ce9c92c457b296472ee4b5474aef7b57aba
Mathis Olaya <150446507+MathisOlaya@users.noreply.github.com>
2026-01-21 13:55:40 +0100

    fix: define db plugin type

--- backend/src/plugins/prisma.ts
+++ backend/src/plugins/prisma.ts
@@ -2,6 +2,11 @@ import fp from "fastify-plugin";
 import { PrismaClient } from "../generated/prisma/client";
 import fastify, { FastifyInstance } from "fastify";
 
+declare module "fastify" {
+  interface FastifyInstance {
+    db: PrismaClient;
+  }
+}
 
 
 export default fp(async (fastify: FastifyInstance) => {
