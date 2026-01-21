dd256e1a91cdee903cf76c51d80b1a7edda2e9e2
eliottscherrer <146830321+eliottscherrer@users.noreply.github.com>
2026-01-21 15:22:02 +0100

    feat: add customSessionClient plugin to authClient configuration

--- frontend/src/lib/authClient.ts
+++ frontend/src/lib/authClient.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
-import { emailOTPClient } from "better-auth/client/plugins";
+import { customSessionClient, emailOTPClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
   baseURL: "http://localhost:3000",
-  plugins: [emailOTPClient()],
+  plugins: [emailOTPClient(), customSessionClient()],
 });
