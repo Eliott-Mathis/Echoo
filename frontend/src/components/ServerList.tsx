71a237cbaa0848f164b4151e84c0d0a607ad7ffb
eliottscherrer <146830321+eliottscherrer@users.noreply.github.com>
2026-01-21 11:17:00 +0100

    fix: adjust ServerList component styling for consistent layout

--- frontend/src/components/ServerList.tsx
+++ frontend/src/components/ServerList.tsx
@@ -2,8 +2,8 @@
 import { Plus } from "lucide-react"
 
 export default function ServerList(){
-    return <div className="p-4 flex flex-col w-fit gap-4 items-center">
-        <img src="/DarkLogo.png" alt="Logo"/>
+    return <div className="p-4 flex flex-col w-20 gap-4 items-center">
+        <img className="w-full" src="/DarkLogo.png" alt="Logo"/>
         <hr className="w-[80%]"/>
         <button className="w-full aspect-square flex justify-center items-center border w-full rounded-full hover:bg-blue-950 transition-colors duration-300"><Plus/></button>
     </div>
