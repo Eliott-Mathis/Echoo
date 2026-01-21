bbefa3f51e7bda8743c64f7329d26d424ba2e160
Mathis Olaya <150446507+MathisOlaya@users.noreply.github.com>
2026-01-21 10:51:53 +0100

    fix: define auth plugins

--- backend/src/plugins/auth.ts
+++ backend/src/plugins/auth.ts
@@ -1,10 +1,14 @@
 
 import fp from "fastify-plugin";
 import type { betterAuth } from "better-auth";
-import { auth} from "../lib/auth";
+import auth from "../lib/auth";
 import { FastifyInstance } from "fastify";
 
-
+declare module "fastify" {
+  interface FastifyInstance {
+    auth: ReturnType<typeof auth>;
+  }
+}
 
 export default fp(async (fastify) => {
   // prisma db instance
