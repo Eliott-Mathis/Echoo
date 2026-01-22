95538b766e6cea595753a0af22ab56d3950e4c17
Eliott Scherrer <146830321+eliottscherrer@users.noreply.github.com>
2026-01-22 15:56:06 +0100

    refactor: standardize import quotes and improve layout in Home component

--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -1,8 +1,8 @@
-import { Outlet } from "react-router-dom";
-import Notification from "@/components/Notification";
+import { Outlet } from 'react-router-dom';
+import Notification from '@/components/Notification';
 
-import ServerList from "@/components/ServerList";
-import FriendsSidebar from "@/components/friends/FriendsSidebar";
+import ServerList from '@/components/ServerList';
+import FriendsSidebar from '@/components/friends/FriendsSidebar';
 export default function Home() {
   return (
     <div className="min-h-screen flex bg-background-primary text-neutral-highest">
@@ -11,11 +11,11 @@ export default function Home() {
       </aside>
       <div className="flex flex-1">
         <FriendsSidebar />
-        <main className="flex-1 flex flex-col">
+        <main className="flex-1 flex flex-col h-screen">
           <Outlet />
         </main>
       </div>
-      <Notification/>
+      <Notification />
     </div>
   );
 }
