5e7c989c33c34c7ecfec93b91e7b10fbef5fa888
Eliott Scherrer <146830321+eliottscherrer@users.noreply.github.com>
2026-01-22 15:07:24 +0100

    refactor: standardize string quotes and improve code formatting in app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,19 +1,19 @@
-import fastify, { FastifyError } from "fastify";
-import cors from "@fastify/cors";
-import formbody from "@fastify/formbody";
-import cookie from "@fastify/cookie";
-import prismaPlugin from "./plugins/prisma";
-import authPlugin from "./plugins/auth";
-import socketPlugin from "./plugins/socket";
-import signupRoutes from "./modules/auth/signup.routes";
-import authRoutes from "./modules/auth/auth.routes";
-import relationshipRoutes from "./modules/relationships/relationships.routes";
+import fastify, { FastifyError } from 'fastify';
+import cors from '@fastify/cors';
+import formbody from '@fastify/formbody';
+import cookie from '@fastify/cookie';
+import prismaPlugin from './plugins/prisma';
+import authPlugin from './plugins/auth';
+import socketPlugin from './plugins/socket';
+import signupRoutes from './modules/auth/signup.routes';
+import authRoutes from './modules/auth/auth.routes';
+import relationshipRoutes from './modules/relationships/relationships.routes';
 
 export const app = fastify({
   logger: true,
 });
 
-const clientOrigin = process.env.CLIENT_ORIGIN ?? "http://localhost:5173";
+const clientOrigin = process.env.CLIENT_ORIGIN ?? 'http://localhost:5173';
 
 // cors policy
 await app.register(cors, {
@@ -33,18 +33,18 @@ await app.register(authPlugin);
 await app.register(socketPlugin);
 
 // signup completion route
-await app.register(signupRoutes, { prefix: "/auth" });
+await app.register(signupRoutes, { prefix: '/auth' });
 
 // Better Auth handler
-await app.register(authRoutes)
+await app.register(authRoutes);
 
 // Relationships routes
-await app.register(relationshipRoutes, { prefix: "/api/relationships" });
+await app.register(relationshipRoutes, { prefix: '/api/relationships' });
 
 // Error hanlder
 app.setErrorHandler((error: FastifyError, request, reply) => {
   request.log.error(error);
   reply.status(error.statusCode || 500).send({
-    error: "Une erreur est survenue",
+    error: 'Une erreur est survenue',
   });
 });
